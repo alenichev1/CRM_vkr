ed21787f022c5cf30fd41bdbec283afac626e138
Александр <106553458+alenichev1@users.noreply.github.com>
2024-02-15 10:55:43 +0300

    Delete server directory

--- server/routes/StatusRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const StatusController = require("../controllers/StatusController");
-
-router.post("/", StatusController.create);
-router.get("/", StatusController.getAll);
-router.delete("/:id", StatusController.destroy);
-
-module.exports = router;
